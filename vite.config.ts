4b283cd4bc7d5b6763ae78620803a0dd58bd28f4
Johnson <chnejohnson@gmail.com>
2022-12-14 14:29:27 +0800

    Successfully preview production build and also run dev server with new configuration only based on node-stdlib-browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import rollupPolyfillNode from 'rollup-plugin-polyfill-node'
 import nodeStdlibBrowser from 'node-stdlib-browser'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	define: {
-		'process.env': {},
-	},
 	plugins: [
 		vue(),
 		// https://github.com/antfu/unplugin-auto-import#configuration
@@ -25,26 +22,26 @@ export default defineConfig({
 		Components({
 			dts: 'src/components.d.ts',
 		}),
+		// https://github.com/niksy/node-stdlib-browser#vite
+		{
+			...inject({
+				global: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'global'],
+				process: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'process'],
+				Buffer: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'Buffer'],
+			}),
+			enforce: 'post',
+		},
 	],
 	resolve: {
-		// Enable polyfill node used in development to prevent from vite's browser compatibility warning
 		alias: { '@': path.resolve(__dirname, 'src'), ...nodeStdlibBrowser },
 	},
 	optimizeDeps: {
-		// Enable polyfill node used in development, refer to https://github.com/sodatea/vite-plugin-node-stdlib-browser/blob/b17f417597c313ecd52c3e420ba8fc33bcbdae20/index.cjs#L17
 		esbuildOptions: {
 			target: 'esnext', // Enable Big integer literals
-			inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
 		},
 	},
 	build: {
 		target: 'esnext', // Enable Big integer literals
-		rollupOptions: {
-			plugins: [
-				// Enable rollup polyfills plugin used in production bundling, refer to https://stackoverflow.com/a/72440811/10752354
-				rollupPolyfillNode(),
-			],
-		},
 		commonjsOptions: {
 			transformMixedEsModules: true, // Enable @walletconnect/web3-provider which has some code in CommonJS
 		},
