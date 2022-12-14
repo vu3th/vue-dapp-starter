9df8fc84909a1ee4476be1e9ea089f25a549d368
Johnson <chnejohnson@gmail.com>
2022-12-14 15:08:15 +0800

    feat: updated with new configuration based on node-stdlib-browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import rollupPolyfillNode from 'rollup-plugin-polyfill-node'
 import nodeStdlibBrowser from 'node-stdlib-browser'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		// https://github.com/niksy/node-stdlib-browser#vite
+		{
+			...inject({
+				global: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'global'],
+				process: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'process'],
+				Buffer: [require.resolve('node-stdlib-browser/helpers/esbuild/shim'), 'Buffer'],
+			}),
+			enforce: 'post',
+		},
+	],
 	resolve: {
-		// Enable polyfill node used in development to prevent from vite's browser compatibility warning
 		alias: { ...nodeStdlibBrowser },
 	},
 	optimizeDeps: {
-		// Enable polyfill node used in development, refer to https://github.com/sodatea/vite-plugin-node-stdlib-browser/blob/b17f417597c313ecd52c3e420ba8fc33bcbdae20/index.cjs#L17
 		esbuildOptions: {
-			inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
+			target: 'esnext', // Enable Big integer literals
 		},
 	},
 	build: {
-		rollupOptions: {
-			plugins: [
-				// Enable rollup polyfills plugin used in production bundling, refer to https://stackoverflow.com/a/72440811/10752354
-				rollupPolyfillNode(),
-			],
-		},
+		target: 'esnext', // Enable Big integer literals
 		commonjsOptions: {
 			transformMixedEsModules: true, // Enable @walletconnect/web3-provider which has some code in CommonJS
 		},
