92f642921f0520eb9090b70768e717e833a4f536
Johnson <chnejohnson@gmail.com>
2023-06-15 22:02:43 +0800

    feat: walletconnect v2

--- src/main.ts
+++ src/main.ts
@@ -10,32 +10,32 @@ const app = createApp(App)
 
 app.use(router)
 app.use(VueDapp, {
-	autoConnect: true,
-	dumb: false,
-	networks: {
-		80001: {
-			chainId: ethers.utils.hexValue(80001),
-			blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
-			chainName: 'Mumbai',
-			rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
-			nativeCurrency: {
-				name: 'Mumbai',
-				decimals: 18,
-				symbol: 'MATIC',
-			},
-		},
-		42161: {
-			chainId: ethers.utils.hexValue(42161),
-			blockExplorerUrls: ['https://arbiscan.io'],
-			chainName: 'Arbitrum One',
-			rpcUrls: ['https://arb1.arbitrum.io/rpc'],
-			nativeCurrency: {
-				name: 'Arbitrum',
-				symbol: 'ETH',
-				decimals: 18,
-			},
-		},
-	},
+  autoConnect: true,
+  dumb: false,
+  networks: {
+    80001: {
+      chainId: ethers.utils.hexValue(80001),
+      blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
+      chainName: 'Mumbai',
+      rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+      nativeCurrency: {
+        name: 'Mumbai',
+        decimals: 18,
+        symbol: 'MATIC',
+      },
+    },
+    42161: {
+      chainId: ethers.utils.hexValue(42161),
+      blockExplorerUrls: ['https://arbiscan.io'],
+      chainName: 'Arbitrum One',
+      rpcUrls: ['https://arb1.arbitrum.io/rpc'],
+      nativeCurrency: {
+        name: 'Arbitrum',
+        symbol: 'ETH',
+        decimals: 18,
+      },
+    },
+  },
 })
 
 app.mount('#app')
