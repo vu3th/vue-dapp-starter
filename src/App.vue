40a1408194c64671261f9da087ffed62b67d5bc4
Johnson Chen <chnejohnson@gmail.com>
2024-04-30 12:54:56 +0800

    vue-dapp v1.2.4

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,31 @@
 <script lang="ts" setup>
-import { BrowserWalletConnector, VueDappProvider, useVueDapp, type ConnWallet, RDNS } from '@vue-dapp/core'
-import { WalletConnectConnector } from '@vue-dapp/walletconnect'
+import { BrowserWalletConnector, useVueDapp } from '@vue-dapp/core'
+import { VueDappModal, useVueDappModal } from '@vue-dapp/modal'
+import '@vue-dapp/modal/dist/style.css'
 
-const { status, isConnected, address, chainId, error, disconnect, connectTo, addConnectors } = useVueDapp()
+const { addConnectors, isConnected, wallet, disconnect } = useVueDapp()
 
-addConnectors([
-	new BrowserWalletConnector(),
-	new WalletConnectConnector({
-		projectId: '3f3c98042b194264687bf59e104c534a',
-		chains: [1],
-		showQrModal: true,
-		qrModalOptions: {
-			themeMode: 'dark',
-			themeVariables: undefined,
-			desktopWallets: undefined,
-			walletImages: undefined,
-			mobileWallets: undefined,
-			enableExplorer: true,
-			privacyPolicyUrl: undefined,
-			termsOfServiceUrl: undefined,
-		},
-	}),
-])
+addConnectors([new BrowserWalletConnector()])
 
-function onClickMetamask() {
-	if (!isConnected.value) {
-		connectTo('BrowserWallet', { rdns: RDNS.metamask })
-	}
-}
-
-function onClickWalletConnect() {
-	if (!isConnected.value) {
-		connectTo('WalletConnect')
-	}
-}
-
-function handleConnect(wallet: ConnWallet) {
-	console.log('handleConnect', wallet)
-}
-
-function handleDisconnect() {
-	console.log('handleDisconnect')
+function onClickConnectButton() {
+	if (isConnected.value) disconnect()
+	else useVueDappModal().open()
 }
 </script>
 
 <template>
-	<div>
-		<VueDappProvider @connect="handleConnect" @disconnect="handleDisconnect">
-			<div v-if="!isConnected" class="connect-btns">
-				<button :disabled="status !== 'idle'" @click="onClickMetamask">Connect with MetaMask</button>
-				<button :disabled="status !== 'idle'" @click="onClickWalletConnect">Connect with WalletConnect</button>
-			</div>
-			<button v-else @click="disconnect">Disconnect</button>
+	<button @click="onClickConnectButton">{{ isConnected ? 'Disconnect' : 'Connect' }}</button>
 
-			<div>status: {{ status }}</div>
-			<div>isConnected: {{ isConnected }}</div>
-			<div>error: {{ error }}</div>
+	<div>status: {{ wallet.status }}</div>
+	<div>isConnected: {{ isConnected }}</div>
+	<div>error: {{ wallet.error }}</div>
 
-			<div v-if="isConnected">
-				<div v-if="chainId">chainId: {{ chainId }}</div>
-				<div>address: {{ address }}</div>
-			</div>
-		</VueDappProvider>
+	<div v-if="isConnected">
+		<div>chainId: {{ wallet.chainId }}</div>
+		<div>address: {{ wallet.address }}</div>
 	</div>
+
+	<VueDappModal dark auto-connect />
 </template>
 
-<style>
-.connect-btns {
-	display: flex;
-	flex-direction: row;
-	gap: 10px;
-}
-</style>
+<style></style>
